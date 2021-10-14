28fc4cba8c996a0f9d9141a5e10e230f0bc8dc9b
Kasper de Graaff <kasperdegraaff1997@gmail.com>
2021-10-14 10:54:41 +0200

    first commit

---
+++ components/coininfo.js
@@ -0,0 +1,5 @@
+const Coininfo = ({nr}) => (
+  `Info van Coin nummer ${nr}`
+)
+
+export default Coininfo
---
+++ components/layout.js
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+
+
+const name = 'Build by Kasper de Graaff'
+export const siteTitle = 'Coininfo.com'
+
+export default function Layout ({children, home}) {
+  return(
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Get info about crypto coins"
+        />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <h1 className={utilStyles.heading2Xl}>{siteTitle}</h1>
+          </>
+        ) : (
+          <>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+    </div>
+
+  )
+}
